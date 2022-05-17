74a7f55d8e11854151bc363d2a1d3a761423d3af
Kalecew <48532006+Kalecew@users.noreply.github.com>
2022-05-17 23:50:24 +0300

    fix bug to portfolio

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,5 +1,4 @@
 // @prepros-append portfolio-load-items.js
-// @prepros-append portfolio-adaptive.js
 // @prepros-append filter-portfolio.js
 // @prepros-append validation-form.js
 // @prepros-append parallax.js
